ff45dae4c59510e98625c030c49464b3d96d79eb
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-18 19:57:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+# construct a object - Turtle
+nadun = Turtle()
+nadun.shape("turtle")
+nadun.color("cyan")
+nadun.fd(100)
+nadun.rt(100)
+nadun.fd(100)
+nadun.lt(100)
+nadun.fd(100)
+
+# construct a object screen
+my_screen = Screen()
+my_screen.bgcolor("red")
+my_screen.exitonclick()
+
