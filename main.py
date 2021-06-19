ae96f209e4d521e71997da17fb1b0736b1d8046c
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-19 15:48:38 +0530

    changes to the main py file

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ my_screen = Screen()
 my_screen.bgcolor("red")
 my_screen.exitonclick()
 
+# pretty table
+from prettytable import PrettyTable
+
+# constructing pretty table object
+table = PrettyTable()
+
+# using methods that are inside the object eg:- add_row()
+table.field_name = ["Pokemon Name", "Type"]
+table.add_row(["Pickachu", "Electric"])
+table.add_row(["Bulbersur", "Grass/Poison"])
+table.add_row(["Charizard", "Fire/Flying"])
+table.add_row(["Weedle", "Bug"])
+
+table.align = "l"
+
+print(table)
