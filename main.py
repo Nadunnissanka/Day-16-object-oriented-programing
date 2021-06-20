976d5d6b6550b7682c64ddd9a7de6e7cde95eabd
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-20 15:19:25 +0530

    Turtle Random Walk

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 from turtle import Turtle, Screen
+import random
 
 turtle = Turtle()
 colors = ["red", "orange", "blue", "green", "purple", "coral", "goldenrod", "lime", "black", "crimson"]
+turtle_width = 10
+turtle.width(turtle_width)
+random_walk = False
+step_count = 0
 
 
-
-def draw_shapes():
-    turtle_color_index = 0
-    sides = 3
-    angel = 120
-    draw = False
-    while not draw:
-        for _ in range(sides):
-            turtle.color(colors[turtle_color_index])
-            turtle.left(angel)
-            turtle.fd(100)
-        turtle_color_index += 1
-        sides += 1
-        angel = 360 / sides
-
-        if sides > 10:
-            draw = True
+# random walk
+def turtle_random_walk():
+    turtle.color(random.choice(colors))
+    random_number = [1, 2]
+    if random.choice(random_number) == 1:
+        turtle.lt(90)
+    else:
+        turtle.rt(90)
+    turtle.forward(50)
 
 
-draw_shapes()
+while not random_walk:
+    if step_count >= 100:
+        random_walk = True
+    else:
+        turtle_random_walk()
+
 
 screen = Screen()
 screen.exitonclick()
